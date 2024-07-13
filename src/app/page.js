67e6c2eed511f3c79dea1bcc1592eada8c828dd1
De-von Nicholson <specktraomega@gmail.com>
2024-07-13 17:10:15 -0300

    created first commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+"use client";
+import "./ToDoCss.css";
+import { ToDoCard } from "./Components/ToDoCard";
+
+
+
+export default function Home() {
+  return (
+    <div className="back_ground"><ToDoCard/></div>
+  )
+   
+}
