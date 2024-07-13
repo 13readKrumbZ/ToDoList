97bcb9a4054996457f7ea28138f5e2872a430982
Bouncy Fish <specktraomega@gmail.com>
2024-07-13 18:04:57 -0300

    prettier format

--- src/app/Components/ToDoCard.js
+++ src/app/Components/ToDoCard.js
@@ -1,39 +1,37 @@
-import { ToDoForm } from "./ToDoForm"
-import { Todo } from "./ToDoTask"; 
-import React, { useState } from "react"
+import { ToDoForm } from "./ToDoForm";
+import { Todo } from "./ToDoTask";
+import React, { useState } from "react";
 
-
-export function ToDoCard(){
-
-  
+export function ToDoCard() {
   const [list, setList] = useState([]);
 
-  function get_Todolist(todolist){
-
-    setList(list => [...list,todolist]);
+  function get_Todolist(todolist) {
+    setList((list) => [...list, todolist]);
     console.log(`${list} hello to you`);
-    
-
   }
 
-  function deleteTask(index){
-    const delList = list.filter((_,idx) => idx !== index)
+  function deleteTask(index) {
+    const delList = list.filter((_, idx) => idx !== index);
     setList(delList);
+  }
 
-  };
-   
-   
-  
-
-    return (
-        <>
-        <div className="card">
-          <ol className="card">{list.map((task, index)=> 
-          <li className="todocell"><li className="todo" key={index}><span>{task}</span>
-          </li><button className="tdbtn" onClick={() => deleteTask(index)}>Delete</button></li>
-        )}</ol>
-          <ToDoForm get_Todolist={get_Todolist}/>
-        </div>
-        </>
-      )
+  return (
+    <>
+      <div className="card">
+        <ol className="card">
+          {list.map((task, index) => (
+            <li className="todocell">
+              <li className="todo" key={index}>
+                <span>{task}</span>
+              </li>
+              <button className="tdbtn" onClick={() => deleteTask(index)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ol>
+        <ToDoForm get_Todolist={get_Todolist} />
+      </div>
+    </>
+  );
 }
--- src/app/Components/ToDoForm.js
+++ src/app/Components/ToDoForm.js
@@ -1,37 +1,33 @@
 import { useState } from "react";
 
+export function ToDoForm({ get_Todolist }) {
+  const [todolist, setTodolist] = useState([]);
+  const [todo, setTodo] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    get_Todolist(todolist);
+    setTodo("");
+    console.log(`${todo} hi`);
+  };
 
+  function addtodo() {
+    setTodolist((todolist) => [todo]);
+  }
 
-export function ToDoForm({get_Todolist}){
-    
-    const [todolist, setTodolist] = useState([]);
-    const [todo, setTodo] = useState("");
-
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        get_Todolist(todolist);
-        setTodo("");
-        console.log(`${todo} hi`);
-    }
-
-    
-        
-      function addtodo(){
-    
-        setTodolist(todolist =>[todo]);
-           
-      };
-    
-    
-
-    return (<form className="form" onSubmit={handleSubmit}>
-        <input id="input" type="text" className="note_input" placeholder="what do we have to do today?"
-        value={todo} onChange={(e)=>setTodo(e.target.value)}/>
-        <button id="subbtn" className="btn" onClick={addtodo} type="submit">Add Note</button>
-        
-        
-        
-    </form>)
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <input
+        id="input"
+        type="text"
+        className="note_input"
+        placeholder="what do we have to do today?"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button id="subbtn" className="btn" onClick={addtodo} type="submit">
+        Add Note
+      </button>
+    </form>
+  );
 }
